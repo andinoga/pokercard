e798790f97f6983601d87db48c39a53f161000cb
Gloria Andino <andinoga@gmail.com>
2019-02-20 01:08:58 +0000

    First commit

---
+++ cards.js
@@ -0,0 +1,24 @@
+
+
+window.onload = () => {
+    document.querySelector(.'card').classList.add('heart');
+    
+};
+
+
+var suite1 : ["hearts", "diamonds"];
+var suite2 : ["spaces", "clubs"];
+
+var nums : [1,2,3,4,5,6,7,8,9,10,J,Q,K]; 
+
+for (suite1 = 0; i<suite1.length; i++) {
+    
+}
+
+for (suite2 = 0; i<suite2.length; i++) {
+    
+}
+
+for (nums = 0; i<nums.length; i++) {
+    
+}
